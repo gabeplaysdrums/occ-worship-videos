5681e2b745bb2b78fffe418f7a6b50d02801cecc
Gabe Y <newsboytko@gmail.com>
2012-07-08 23:42:59 -0700

    send mail when download fails with log file

--- scripts/download.py
+++ scripts/download.py
@@ -23,8 +23,9 @@ config = ConfigParser()
 config.read(sys.argv[1])
 
 # create output directory
+output_directory_name = compute_datetime_path_string()
 output_directory = '%s/%s' % (
-  config.get('general', 'root_directory'), compute_datetime_path_string()
+  config.get('general', 'root_directory'), output_directory_name
 )
 if not os.path.exists(output_directory):
   os.makedirs(output_directory)
@@ -64,66 +65,88 @@ poll_interval = parse_timedelta(config.get('recording', 'poll_interval'))
 min_duration = parse_timedelta(config.get('recording', 'min_duration'))
 duration = None
 record_succeeded = False
+mail_subject = 'OCC stream download %s' % output_directory_name
 
 send_mail(
-  subject='OCC stream download started',
+  subject=mail_subject,
   body="""
-    Started downloading the OCC media stream.  Output is saved to '%s'.
-  """ % (
+Started downloading the OCC media stream.  Output is saved to '%s'.
+""" % (
     output_file_path
   )
 )
 
-for tries_remaining in range(poll_tries, 0, -1):
-  try:
-    # record the stream
-    start_time = datetime.now()
-    command = (
-      'rtmpdump -v -y broadcast1 -r "rtmp://fss24.streamhoster.com/lv_occvideof1" -o %s -s "http://public.streamhoster.com/Resources/Flash/JWFLVMediaPlayer/mediaplayer.swf" -w 8ac08c568ab193b9e6d82ee9c0f6430a773f372a6afe6ef1ae735d58278430cd -x 50076' % (
-        output_file_path
+try:
+
+  for tries_remaining in range(poll_tries, 0, -1):
+    try:
+      # record the stream
+      start_time = datetime.now()
+      command = (
+        'rtmpdump -v -y broadcast1 -r "rtmp://fss24.streamhoster.com/lv_occvideof1" -o %s -s "http://public.streamhoster.com/Resources/Flash/JWFLVMediaPlayer/mediaplayer.swf" -w 8ac08c568ab193b9e6d82ee9c0f6430a773f372a6afe6ef1ae735d58278430cd -x 50076' % (
+          output_file_path
+        )
       )
+      result = run_and_log(command)
+      if (result != 0):
+        raise Exception('unexpected exit code: %d' % result)
+      end_time = datetime.now()
+      
+      # ensure the duration matches the expected value
+      duration = (end_time - start_time)
+      logger.info('finished recording, duration: %s' % duration)
+      if (duration < min_duration):
+        raise Exception(
+          'duration is less than the minimum value expected (%s)' % 
+          min_duration
+        )
+    except Exception as error:
+      logger.info('failed to record the stream: %s' % error)
+      logger.info('trying again in %s (%d tries left) ...' % (
+        poll_interval, tries_remaining
+      ))
+      sleep(poll_interval.seconds)
+      continue
+    record_succeeded = True
+    break
+  
+  if (not record_succeeded):
+    raise Exception(
+      'Failed to record stream after several attempts.  Giving up.'
     )
-    result = run_and_log(command)
-    if (result != 0):
-      raise Exception('unexpected exit code: %d' % result)
-    end_time = datetime.now()
-    
-    # ensure the duration matches the expected value
-    duration = (end_time - start_time)
-    logger.info('finished recording, duration: %s' % duration)
-    if (duration < min_duration):
-      raise Exception(
-        'duration is less than the minimum value expected (%s)' % 
-        min_duration
-      )
-  except Exception as error:
-    logger.info('failed to record the stream: %s' % error)
-    logger.info('trying again in %s (%d tries left) ...' % (
-      poll_interval, tries_remaining
-    ))
-    sleep(poll_interval.seconds)
-    continue
-  record_succeeded = True
-  break
-
-if (not record_succeeded):
-  raise Exception(
-    'Failed to record stream after several attempts.  Giving up.'
+  
+  logger.info('Finished recording stream')
+  
+  logger.info('Dumping frames to assist with editing')
+  dump_frames(input_file=output_file_path, end=duration)
+  
+  send_mail(
+    subject=mail_subject,
+    body="""
+Downloaded the OCC media stream successfully.  Output is saved to '%s'.
+""" % (
+      output_file_path
+    )
   )
+  
+  logger.info('success!')
 
-logger.info('Finished recording stream')
+except Exception as error:
 
-logger.info('Dumping frames to assist with editing')
-dump_frames(input_file=output_file_path, end=duration)
+  logger.error('Failed to download the stream: %s' % error)
+  logfile = open(logfile_path, 'r')
+  send_mail(
+    subject=mail_subject,
+    body="""
+Failed to download the OCC media stream.  Failed with error message '%s'
 
-send_mail(
-  subject='OCC stream download finished',
-  body="""
-    Downloaded the OCC media stream successfully.  Output is saved to '%s'.
-  """ % (
-    output_file_path
+Here is the log file:
+===============================================================================
+%s
+===============================================================================
+""" % (
+      error, logfile.read()
+    )
   )
-)
-
-logger.info('success!')
-
+  logfile.close()
+  raise error
